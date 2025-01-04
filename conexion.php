22b79706dd4042e167c9b16fb5bca3caf8260f3e
AlejoxCL <alejox19@gmail.com>
2025-01-04 16:51:30 -0300

    cambio a conexion y estilos

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";  // o el host que corresponda
-$username = "root";         // usuario de la BD
-$password = "root";             // contraseña de la BD
-$dbname = "GOURMET";        // nombre de la base de datos
+$servername = "localhost";  
+$username = "root";         
+$password = "root";           
+$dbname = "GOURMET";    
 
 // Crear la conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,6 +12,4 @@ if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
 
-// Opcional: establecer set names UTF8 si se quiere para acentos, etc.
-// $conn->set_charset("utf8");
 ?>
